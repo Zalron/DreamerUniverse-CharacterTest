742190da00e1647159fcbf6238a62ee1f2e83d62
Zalron <davidrlawson@optusnet.com.au>
2021-08-23 16:41:36 +1000

    Made Character a module

--- CharacterTest/CharacterTest/Characters.h
+++ CharacterTest/CharacterTest/Characters.h
@@ -1,58 +1,72 @@
 #pragma once
-struct CharacterAttributes
-{
-	int Strength;
-	int Dexterity;
-	int Intelligence;
-	int Willpower;
-	int Endurance;
-	int Luck;
-};
-struct CharacterStats
-{
-	int Life;
-	float LifeRegenRate;
-	float LifeRegenDelay;
-	int Energy;
-	float EnergyRegenRate;
-	float EnergyRegenDelay;
-	int Armour;
-	float ArmourRegenRate;
-	float ArmourRegenDelay;
-	int Shield;
-	float ShieldRegenRate;
-	float ShieldRegenDelay;
-	int Magic;
-	float MagicRegenRate;
-	float MagicRegenDelay;
-};
-struct CharacterResistenances 
-{
-	float MagicRadiationResistance;
-	float MagicInsanityResistance;
-	float MagicStiffnessResistance;
-};
+#include "flecs.h"
 
-struct CharacterNumbers 
+struct Characters
 {
-	int addFromGearMods, addFromTreeMods;
-	int minusFromGearMods, minusFromTreeMods;
-	int flatTotal;
-	int increasedFromGearMods, increasedFromTreeMods;
-	int decreasedFromGearMods, decreasedFromTreeMods;
-	int additivePercentageTotal;
-	int moreFromGearMods, moreFromTreeMods;
-	int lessFromGearMods, lessFromTreeMods;
-	int total;
+	struct CharacterAttributes
+	{
+		int Strength;
+		int Dexterity;
+		int Intelligence;
+		int Willpower;
+		int Endurance;
+		int Luck;
+	};
+
+	struct CharacterStats
+	{
+		int Life;
+		float LifeRegenRate;
+		float LifeRegenDelay;
+		int Energy;
+		float EnergyRegenRate;
+		float EnergyRegenDelay;
+		int Armour;
+		float ArmourRegenRate;
+		float ArmourRegenDelay;
+		int Shield;
+		float ShieldRegenRate;
+		float ShieldRegenDelay;
+		int Magic;
+		float MagicRegenRate;
+		float MagicRegenDelay;
+	};
+
+	struct CharacterResistenances 
+	{
+		float MagicRadiationResistance;
+		float MagicInsanityResistance;
+		float MagicStiffnessResistance;
+	};
+
+	struct CharacterStatNumbers 
+	{
+		int addFromGearMods, addFromTreeMods;
+		int minusFromGearMods, minusFromTreeMods;
+		int flatTotal;
+		float increasedFromGearMods, increasedFromTreeMods;
+		float decreasedFromGearMods, decreasedFromTreeMods;
+		float additivePercentageTotal;
+		float moreFromGearMods, moreFromTreeMods;
+		float lessFromGearMods, lessFromTreeMods;
+		float multiplicativePercentageTotal;
+		float total;
+	};
+
+	struct PassiveTreeModNumbers
+	{
+		int intmod;
+		float floatmod;
+	};
+
+	Characters(flecs::world& world) 
+	{
+		world.module<Characters>();
+
+		world.component<CharacterAttributes>();
+		world.component<CharacterStats>();
+		world.component<CharacterResistenances>();
+		world.component<CharacterStatNumbers>();
+		world.component<PassiveTreeModNumbers>();
+	}
 };
-
-struct PassiveTreeMod 
-{
-
-};
-
-class Characters
-{
-
-};
-
