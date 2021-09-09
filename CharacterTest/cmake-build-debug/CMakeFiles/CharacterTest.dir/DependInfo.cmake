3a74c6196cdda7a25f378360bd9000960415618f
Zalron <davidrlawson@optusnet.com.au>
2021-09-09 14:32:01 +1000

    Added cmake to the project

---
+++ CharacterTest/cmake-build-debug/CMakeFiles/CharacterTest.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/David Work/Desktop/Dreamer Universe projects/DreamerUniverse-CharacterTest/CharacterTest/CharacterTest/flecs.c" "C:/Users/David Work/Desktop/Dreamer Universe projects/DreamerUniverse-CharacterTest/CharacterTest/cmake-build-debug/CMakeFiles/CharacterTest.dir/CharacterTest/flecs.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../CharacterTest"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/David Work/Desktop/Dreamer Universe projects/DreamerUniverse-CharacterTest/CharacterTest/CharacterTest/Characters.cpp" "C:/Users/David Work/Desktop/Dreamer Universe projects/DreamerUniverse-CharacterTest/CharacterTest/cmake-build-debug/CMakeFiles/CharacterTest.dir/CharacterTest/Characters.cpp.obj"
+  "C:/Users/David Work/Desktop/Dreamer Universe projects/DreamerUniverse-CharacterTest/CharacterTest/CharacterTest/Main.cpp" "C:/Users/David Work/Desktop/Dreamer Universe projects/DreamerUniverse-CharacterTest/CharacterTest/cmake-build-debug/CMakeFiles/CharacterTest.dir/CharacterTest/Main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../CharacterTest"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CharacterTest/cmake-build-debug/CMakeFiles/CharacterTest.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CharacterTest.dir/CharacterTest/Characters.cpp.obj"
+  "CMakeFiles/CharacterTest.dir/CharacterTest/Main.cpp.obj"
+  "CMakeFiles/CharacterTest.dir/CharacterTest/flecs.c.obj"
+  "CharacterTest.exe"
+  "CharacterTest.exe.manifest"
+  "CharacterTest.lib"
+  "CharacterTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/CharacterTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
